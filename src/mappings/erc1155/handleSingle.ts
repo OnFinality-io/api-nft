2c9709b5643f9db0183a21659af816ed3ccdfbdc
bz888 <ziyu.ben158@gmail.com>
2023-05-19 15:18:18 +1200

    removed redundant interface check;

--- src/mappings/erc1155/handleSingle.ts
+++ src/mappings/erc1155/handleSingle.ts
@@ -11,16 +11,6 @@ import { BigNumber } from 'ethers';
 export async function handleERC1155Single(
   event: TransferSingleLog
 ): Promise<void> {
-  const instance = Erc1155__factory.connect(event.address, api);
-  try {
-    // https://eips.ethereum.org/EIPS/eip-1155#abstract
-    const isERC1155 = await instance.supportsInterface('0xd9b67a26');
-    if (!isERC1155) {
-      return;
-    }
-  } catch (e) {
-    return;
-  }
   assert(event.args, 'No event args on erc1155');
   const [operator, from, to, id, value] = event.args;
   const newArgs: [string, string, string, BigNumber[], BigNumber[]] = [
