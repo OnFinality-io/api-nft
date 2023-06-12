eb0025ca25582de6f67a1d3bb6acf29d0354644a
bz888 <ziyu.ben158@gmail.com>
2023-06-12 14:31:46 +1200

    update blacklist addresses, update astar cid

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,6 @@
 /*
 These addresses are blacklisted as they are proxy contracts that implements multiple interfaces, and does not upgrade to ERC721 or ERC1155
  */
-
-// Get list from db
 export const blackListedAddresses = [
   '0x969d515486ba6133400ac9c73298586426a090f5', // 592, 1285
   '0xa5bd5c661f373256c0ccfbc628fd52de74f9bb55', // 1284
@@ -15,4 +13,5 @@ export const blackListedAddresses = [
   '0xfe8874778f946ac2990a29eba3cfd50760593b2f', // 1284
   '0x2c78f1b70ccf63cdee49f9233e9faa99d43aa07e', // 1285
   '0x0e4024950d71f630d710f8523e4e40656a2cd617', // 592
+  '0x30510d8a06fb680370547fd42d5bcc127f3930c1', // 592
 ];
